38125f059737fb92f62b987fbf470a80f52c0055
melwin3210 <melwin3210@gmail.com>
2025-08-20 18:09:35 +0530

    react router dom config

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='p-8'>
+      <h1 className='text-3xl font-bold mb-4'>Contact Us</h1>
+      <p className='text-gray-600'>Get in touch with us!</p>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
         <div className="h-13 w-full border-4 bg-gray-300 flex justify-between">
             <img src="https://www.48hourslogo.com/48hourslogo_data/2014/01/20/2014012015281119637.png" alt="" className="h-full"/>
             <div>
-            <a className="m-2" href="">Home</a>
-            <a className="m-2" href="">About us</a>
-            <a className="m-2" href="">Contact</a>
-            
+                <Link className="m-2 hover:text-blue-600" to="/">Home</Link>
+                <Link className="m-2 hover:text-blue-600" to="/about">About us</Link>
+                <Link className="m-2 hover:text-blue-600" to="/contact">Contact</Link>
             </div>
         </div>
     )
