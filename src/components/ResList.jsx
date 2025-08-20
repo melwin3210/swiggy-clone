8a276f17312d7162d81de4b2e8160699be7ed17c
melwin3210 <melwin3210@gmail.com>
2025-08-20 17:28:11 +0530

    top rated re filter added

--- src/components/ResList.jsx
+++ src/components/ResList.jsx
@@ -4,6 +4,7 @@ import ResCard from './ResCard'
 const ResList = () => {
     const [resLists,setResLists] = useState([])
     const [filteredResLists,setFilteredResLists] = useState([])
+    const [topRated,setTopRated] = useState(false)
     const [searchInput, setSearchInput]= useState("")
     useEffect(()=>{
         console.log("useEffect called")
@@ -18,6 +19,21 @@ const ResList = () => {
         setFilteredResLists(resLists)
 
     }
+    useEffect(()=>{
+        
+            topRatedRes(topRated)
+        
+    },[topRated])
+    const topRatedRes = (e) => {
+        if(!e){
+            setFilteredResLists(resLists)
+            return
+        }
+        const filteredList = resLists.filter((resInfo)=>{
+            return resInfo.info.avgRating>4.5
+        })
+        setFilteredResLists(filteredList)
+    }
     const filterSearchList = () => {
         const filteredList = resLists.filter((resInfo)=>{
             return resInfo.info.name.toLowerCase().includes(searchInput.toLowerCase())
@@ -29,6 +45,12 @@ const ResList = () => {
     <div>
         <input className='border-1 border-grey-100' type="text" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)} />
         <button className='bg-gray-100 cursor-pointer ml-2' onClick={filterSearchList}>Search</button>
+        <button className='bg-gray-100 cursor-pointer ml-2' onClick={()=>{
+            setFilteredResLists(resLists)
+            setSearchInput("")
+        }}>Clear</button>
+        <input type="checkbox" aria-label='Top Rated' value={topRated} onChange={(e)=>setTopRated(prev => !prev)} />
+        <label htmlFor="Top Rated">Top Rated</label>
     </div>
     <div className='flex flex-wrap justify-center'>
         {filteredResLists.map((resInfo)=>{
